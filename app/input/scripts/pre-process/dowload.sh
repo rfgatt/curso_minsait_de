324094fc07884cb0578741e4dc6892744df4f8ae
rfgatt <rfortesgatto@gmail.com>
2023-05-20 21:01:29 +0000

    commit

---
+++ app/input/scripts/pre-process/dowload.sh
@@ -0,0 +1,44 @@
+cd ../../raw
+mkdir categoria
+cd categoria
+curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/categoria.csv
+
+cd ../../raw
+mkdir cidade
+cd cidade
+curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/cidade.csv
+
+cd ../../raw
+mkdir cliente
+cd cliente
+curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/cliente.csv
+
+cd ../../raw
+mkdir estado
+cd estado
+curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/estado.csv
+
+cd ../../raw
+mkdir filial
+cd filial
+curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/filial.csv
+
+cd ../../raw
+mkdir item_pedido
+cd item_pedido
+curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/item_pedido.csv
+
+cd ../../raw
+mkdir parceiro
+cd parceiro
+curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/parceiro.csv
+
+cd ../../raw
+mkdir produto
+cd produto
+curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/produto.csv
+
+cd ../../raw
+mkdir subcategoria
+cd subcategoria
+curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/subcategoria.csv
